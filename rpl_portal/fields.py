1a17202d34a2b14111185ebdf1813a37064d8ac5
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2023-03-15 18:40:27 -0500

    upgraded publishing system and display

--- rpl_portal/fields.py
+++ rpl_portal/fields.py
@@ -6,8 +6,16 @@ import re
 def title(result):
     if not result[0].get("dc"):
         return
-    """The title for this Globus Search subject"""
-    return result[0]["dc"]["titles"][0]["title"]
+    t = result[0]["project_metadata"]
+    if not ("experiment" in t):
+        """The title for this Globus Search subject"""
+        
+        
+        return result[0]["dc"]["titles"][0]["title"]
+   
+
+    return result[0]["project_metadata"]["experiment"]
+
 
 
 def all_files(result):
